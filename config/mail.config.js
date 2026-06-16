680982a327762b57b7d3b313f10689d0719c3082
Nitin verma <nv5327260@gmail.com>
2026-06-16 18:49:56 +0530

    add oen

--- config/mail.config.js
+++ config/mail.config.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
 
-console.log("doneee")
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-    auth:{
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
-    }
-})
-
+    },
+});
 
 const sendMail = async ({ to, subject, html }) => {
     console.log('to, subject, html', to, subject, html)
