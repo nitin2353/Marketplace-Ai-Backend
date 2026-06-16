ef2a4f6f1f7f3481e9248df180554d6f83e87712
Nitin verma <nv5327260@gmail.com>
2026-06-16 18:43:13 +0530

    add oen

--- config/mail.config.js
+++ config/mail.config.js
@@ -1,16 +1,19 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
 
-
+console.log("doneee")
 const transporter = nodemailer.createTransport({
   service: 'gmail',
     auth:{
-         user: process.env.MAIL_USER,
+        user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
     }
 })
+console.log("transporter", transporter)
+
 
 const sendMail = async ({ to, subject, html }) => {
+    console.log('to, subject, html', to, subject, html)
     return await transporter.sendMail({
         from: `"${process.env.MAIL_FROM_NAME || "ShopEase"}" <${process.env.MAIL_FROM_EMAIL || process.env.MAIL_USER}>`,
         to,
